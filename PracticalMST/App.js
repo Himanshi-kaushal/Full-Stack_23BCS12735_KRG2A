b60220ca097651b0499309aa63e2eefde528b8b4
Himanshi-kaushal <himanshikaushal049@gmail.com>
2025-10-08 12:01:07 +0530

    Add files via upload

---
+++ PracticalMST/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import Ques1 from "./components/ques1";
+import Ques2 from "./components/ques2";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>React Assessment</h1>
+      <Ques1 />
+      <Ques2 />
+    </div>
+  );
+}
+
+export default App;
