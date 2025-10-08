1204ad53292a143302354487a98bd0971861dbc5
Himanshi-kaushal <himanshikaushal049@gmail.com>
2025-10-08 12:39:50 +0530

    Create App.jsx

---
+++ Experiment_4 (Frontend)/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 py-6">
+        <AppRoutes />
+      </main>
+      <Footer />
+    </div>
+  );
+}
